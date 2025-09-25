a6294e19f034a3ab907f02e8151c16aa34652116
Michele Mazzucco <hello@michelemazzucco.it>
2025-09-25 09:38:55 +0200

    polishing and cleanup

--- app/components/ApiDialog/index.tsx
+++ app/components/ApiDialog/index.tsx
@@ -5,6 +5,7 @@ import { useState } from 'react'
 import { Dialog } from '@base-ui-components/react/dialog'
 import { Button, KeyField } from '../'
 import styles from './index.module.css'
+import { useToast } from '../../hooks/useToast'
 
 export const ApiDialog = ({
   open,
@@ -18,8 +19,13 @@ export const ApiDialog = ({
   setApiKey: (apiKey: string) => void
 }) => {
   const [localApiKey, setLocalApiKey] = useState(apiKey)
+  const { showToast } = useToast()
 
   const handleSave = () => {
+    if (!localApiKey) {
+      showToast('Please add you API key :)')
+      return
+    }
     setApiKey(localApiKey)
     setOpen(false)
   }
@@ -29,18 +35,27 @@ export const ApiDialog = ({
       <Dialog.Portal>
         <Dialog.Backdrop className={styles.backdrop} />
         <Dialog.Popup className={styles.popup}>
-          <Dialog.Title className={styles.title}>Enter OpenAI API key</Dialog.Title>
+          <Dialog.Title className={styles.title}>
+            <div className={styles.emoji}>🫣</div>
+            Enter your OpenAI API key
+          </Dialog.Title>
           <Dialog.Description className={styles.description}>
             To get started, please enter your API key &mdash; It will not be saved anywhere.
           </Dialog.Description>
           <div className={styles.content}>
-            <KeyField value={localApiKey} onChange={setLocalApiKey} />
+            <KeyField
+              id="apikey"
+              type="password"
+              value={localApiKey}
+              onChange={(e) => setLocalApiKey(e.target.value)}
+              placeholder="sk-..."
+            />
           </div>
           <div className={styles.actions}>
             <Button variant="text" onClick={() => setOpen(false)}>
               Cancel
             </Button>
-            <Button variant="primary" onClick={handleSave} disabled={!localApiKey}>
+            <Button variant="primary" onClick={handleSave}>
               Get Started
             </Button>
           </div>
--- app/components/KeyField/index.tsx
+++ app/components/KeyField/index.tsx
@@ -1,31 +1,5 @@
-import * as PasswordToggleField from '@radix-ui/react-password-toggle-field'
-import { EyeIcon, EyeSlashIcon } from '@heroicons/react/16/solid'
-
 import styles from './index.module.css'
 
-export const KeyField = ({
-  value,
-  onChange,
-  placeholder,
-}: {
-  value: string
-  onChange: (value: string) => void
-  placeholder?: string
-}) => (
-  <PasswordToggleField.Root>
-    <div className={styles.Root}>
-      <PasswordToggleField.Input
-        className={styles.Input}
-        value={value}
-        onChange={(e) => onChange(e.target.value)}
-        placeholder={placeholder}
-      />
-      <PasswordToggleField.Toggle className={styles.Toggle}>
-        <PasswordToggleField.Icon
-          visible={<EyeIcon fill="#FFF" width={16} height={16} />}
-          hidden={<EyeSlashIcon fill="#FFF" width={16} height={16} />}
-        />
-      </PasswordToggleField.Toggle>
-    </div>
-  </PasswordToggleField.Root>
+export const KeyField = (props: React.ComponentProps<'input'>) => (
+  <input className={styles.input} {...props} />
 )
